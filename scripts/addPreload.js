350b7c5a67032e5f9e55905c081531967a89d954
sesn <sesankar11@gmail.com>
2018-06-04 17:56:45 +0530

    added critical module.

--- scripts/addPreload.js
+++ scripts/addPreload.js
@@ -1,32 +1,51 @@
-const fs = require('fs');
-const path = require('path');
-const packageJson = require('../package.json');
-const pathToEntry = './build/index.html';
-const bundlesRegExp = /\/static\/\w+\/\w+.[a-z0-9]+.\w{2,3}/g;
-const builtHTMLContent = fs.readFileSync(path.resolve(pathToEntry)).toString();
-const links = builtHTMLContent.match(bundlesRegExp);
+const critical = require('critical');
 
-let changedHTMLContent;
-
-
-// To fix the slash issue if path add extra slash on the end or not
-let homepagePath = (!!packageJson.homepage) ? packageJson.homepage.split('/').filter(st => st).join('/') : '';
-
-links.forEach(link => {
-    let fileType, fromReplace;
-    // Check whether the file is css or not
-    if(/\.css$/.test(link)) {
-        fileType = 'style';
-        fromReplace = `<link href="${homepagePath}${link}" rel="stylesheet">`;
-    } 
-    // Check whether the file is script or not
-    else if(/\.js$/.test(link)) {
-        fileType = 'script';
-        fromReplace = `<script type="text/javascript" src="${homepagePath}${link}"></script>`;
-    }
-
-    changedHTMLContent = builtHTMLContent.replace(fromReplace, `<link rel="preload" href="${homepagePath}${link}" as="${fileType}">`);
+critical.generate({
+    inline: true,
+    base: 'build',
+    src: 'index.html',
+    dest: 'index.html',
+    minify: true,
+    width: 1300,
+    height: 900,
+    ignore: ['@font-face',/url\(/]
 });
 
+/**
+ * 
+ * These functionality is provided by critical npm package while generating the 
+ * critical css itself
+ * 
+ */
 
-// fs.writeFileSync(pathToEntry, fileWithPreload.join(''));
+// const fs = require('fs');
+// const path = require('path');
+// const packageJson = require('../package.json');
+// const pathToEntry = './build/index.html';
+// const bundlesRegExp = /\/static\/\w+\/\w+.[a-z0-9]+.\w{2,3}/g;
+// const builtHTMLContent = fs.readFileSync(path.resolve(pathToEntry)).toString();
+// const links = builtHTMLContent.match(bundlesRegExp);
+
+// let changedHTMLContent;
+
+
+// // To fix the slash issue if path add extra slash on the end or not
+// let homepagePath = (!!packageJson.homepage) ? packageJson.homepage.split('/').filter(st => st).join('/') : '';
+
+// links.forEach(link => {
+//     let fileType, fromReplace;
+//     // Check whether the file is css or not
+//     if(/\.css$/.test(link)) {
+//         fileType = 'style';
+//         fromReplace = `<link href="${homepagePath}${link}" rel="stylesheet">`;
+//     } 
+//     // Check whether the file is script or not
+//     else if(/\.js$/.test(link)) {
+//         fileType = 'script';
+//         fromReplace = `<script type="text/javascript" src="${homepagePath}${link}"></script>`;
+//     }
+
+//     changedHTMLContent = builtHTMLContent.replace(fromReplace, `<link rel="preload" href="${homepagePath}${link}" as="${fileType}">`);
+// });
+
+// fs.writeFileSync(pathToEntry, changedHTMLContent);
